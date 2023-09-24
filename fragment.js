c811013e2c103ae5f9fe75b1526a441e61ec1bbc
Murphy Lopes <murphymlopes@student.midlandstech.edu>
2023-09-23 23:23:52 -0400

    test move unity instance creation to fragment.js

--- fragment.js
+++ fragment.js
@@ -8,4 +8,19 @@
 function playFragment() {
     document.getElementById("fragmentGameDiv").style.display = "flex";
     document.getElementById("fragmentControlsDiv").style.display = "none";
+    createUnityInstance
+    (
+        document.querySelector("#unity-canvas"), 
+        {
+            dataUrl: "Build/FragmentAlphaWebGL.data",
+            frameworkUrl: "Build/FragmentAlphaWebGL.framework.js",
+            codeUrl: "Build/FragmentAlphaWebGL.wasm",
+            streamingAssetsUrl: "StreamingAssets",
+            companyName: "RoundTableGames",
+            productName: "Fragment",
+            productVersion: "1.0.1",
+            // matchWebGLToCanvasSize: false, // Uncomment this to separately control WebGL canvas render size and DOM element size.
+            // devicePixelRatio: 1, // Uncomment this to override low DPI rendering on high DPI displays.
+        }
+    );
 }
