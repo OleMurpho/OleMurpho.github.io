eba7c4a2ffc08254971d72c4b16037429c26a3b2
Murphy Lopes <murphymlopes@student.midlandstech.edu>
2023-09-23 23:06:17 -0400

    tighten fragment.js

--- fragment.js
+++ fragment.js
@@ -5,14 +5,7 @@
  */
 "use strict";
 
-//dollar store jQuery.
-var $ = function(id) { return document.getElementById(id); };
-
-var fragmentPlayButton = $("fragmentPlayButton");
-var fragmentControlsDiv = $("fragmentControlsDiv");
-var fragmentGameDiv = $("fragmentGameDiv");
-
 function playFragment() {
-    fragmentGameDiv.style.display = "flex";
-    fragmentControlsDiv.style.display = "none";
+    document.getElementById("fragmentGameDiv").style.display = "flex";
+    document.getElementById("fragmentControlsDiv").style.display = "none";
 }
